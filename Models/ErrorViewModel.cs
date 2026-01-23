7614f5d4082aa48d95fe6d20f5a1a95f688838e4
KacperMajerski <kacper.majerski2002@gmail.com>
2026-01-23 14:47:49 +0100

    Dodaj pliki projektów.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KinowaRezerwacja.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
