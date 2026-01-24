9d08a7ddc89338c49503cc985ed368ba6fd90502
KacperMajerski <kacper.majerski2002@gmail.com>
2026-01-24 04:34:27 +0100

    Tworzenie seansów

---
+++ Controllers/SeancesController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using KinowaRezerwacja.Data;
+using KinowaRezerwacja.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace KinowaRezerwacja.Controllers
+{
+
+
+    [Authorize(Roles = "Admin")]
+    public class SeancesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SeancesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Seances
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Seances.Include(s => s.Hall).Include(s => s.Movie);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Seances/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var seance = await _context.Seances
+                .Include(s => s.Hall)
+                .Include(s => s.Movie)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (seance == null)
+            {
+                return NotFound();
+            }
+
+            return View(seance);
+        }
+
+        // GET: Seances/Create
+        public IActionResult Create()
+        {
+            ViewData["HallId"] = new SelectList(_context.Halls, "Id", "Name");
+            ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Title");
+            return View();
+        }
+
+        // POST: Seances/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Seance seance)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(seance);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["HallId"] = new SelectList(_context.Halls, "Id", "Name", seance.HallId);
+            ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Title", seance.MovieId);
+            return View(seance);
+        }
+
+        // GET: Seances/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var seance = await _context.Seances.FindAsync(id);
+            if (seance == null)
+            {
+                return NotFound();
+            }
+            ViewData["HallId"] = new SelectList(_context.Halls, "Id", "Name", seance.HallId);
+            ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Title", seance.MovieId);
+            return View(seance);
+        }
+
+        // POST: Seances/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Seance seance)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(seance);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SeanceExists(seance.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["HallId"] = new SelectList(_context.Halls, "Id", "Name", seance.HallId);
+            ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Title", seance.MovieId);
+            return View(seance);
+        }
+
+        // GET: Seances/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var seance = await _context.Seances
+                .Include(s => s.Hall)
+                .Include(s => s.Movie)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (seance == null)
+            {
+                return NotFound();
+            }
+
+            return View(seance);
+        }
+
+        // POST: Seances/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var seance = await _context.Seances.FindAsync(id);
+            if (seance != null)
+            {
+                _context.Seances.Remove(seance);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SeanceExists(int id)
+        {
+            return _context.Seances.Any(e => e.Id == id);
+        }
+    }
+}
